4be4ee44dcbd00fe11c1e136bcfe2a605a3740ea
Blake Conrad <bmconrad@iupui.edu>
2018-07-01 11:56:33 -0500

    Add files via upload

--- leaderboard.py
+++ leaderboard.py
@@ -1,7 +1,20 @@
+
+import sys
+sys.path.append("/streamml")
+from streamml.streamline.model_selection.flow.ModelSelectionStream import ModelSelectionStream
+from streamml.streamline.transformation.flow.TransformationStream import TransformationStream
+
 from src.models.model_template import ModelMyModel
 from src.models.model_correlation_grouper import ModelCorrelationGrouper
-from src.models.model_support_vector_regressor import ModelSupportVectorRegressor
+from src.models.model_linear_reg import ModelLinearReg
+from src.models.model_sgd_regression import ModelSGDRegressor
+from src.models.model_mixed_stepwise import ModelMixedStepwise
+from src.models.model_linear_svr import ModelLinearSVR
+from src.models.model_best_svr import ModelBestSVR
+from src.models.topsis_mlpr import ModelTOPSISMLPR
 from src.model_validation import ModelValidation
+import numpy as np
+np.set_printoptions(suppress=True)
 
 """
 Standard leaderboard.py implementation
@@ -14,9 +27,13 @@ command line with `python leaderboard.py` from the project root.
 validation = ModelValidation()
 
 leaderboard_regressors = [
+    #ModelMixedStepwise(),
+    ModelBestSVR(),
     #ModelMyModel(),
-    #ModelCorrelationGrouper(),
-    ModelSupportVectorRegressor()
+    #ModelLinearReg(),
+    #ModelSGDRegressor(),
+    ModelLinearSVR(),
+    ModelTOPSISMLPR()
     # TODO add additional regression implementations
 ]
 leaderboard_reg_scores = []
@@ -24,16 +41,19 @@ leaderboard_reg_scores = []
 
 def score():
     for model_class in leaderboard_regressors:
-        print("Running %s" % model_class.__class__.__name__ + "....")
+        print("Running %s" % model_class.__class__.__name__ + "....\n")
         x_train, x_test, y_train, y_scaler, model = model_class.get_validation_support()
+        print("\nScore Summary:")
         validation_result = validation.score_regressor(
-            x_train, y_train, model,
+            x_train, y_train, model, y_scaler,
             pos_split=y_scaler.transform([[2.1]])
         )
         leaderboard_reg_scores.append(validation_result)
-        print()
-        print("Test Prediction:")
-        print(model_class.get_test_prediction())
+        print("\nTest Prediction:")
+        # print(y_scaler.inverse_transform(model.fit(x_train, y_train).predict(x_test)))
+        predictions = y_scaler.inverse_transform(model.fit(x_train, y_train).predict(x_test))
+        for compound, pred in zip(x_test.index, predictions):
+            print("{:<10}".format(compound), ": ", '{0:>5.2f}'.format(pred))
         print("\n")
 
 
